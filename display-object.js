dfd32a2974cf2bb19553cd46ab857aabb8739cee
Jessica Leach <jessicamaryleach@gmail.com>
2017-07-14 14:26:01 +0200

    separate files

--- display-object.js
+++ display-object.js
@@ -1,25 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Index from '../index.js';
+import Index from '../index-pouch.js';
 
 
-class App extends Component {
+class Notifs extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <pre id="display"></pre>
         <Index />
       </div>
     );
   }
 }
 
-export default App;
+export default Notifs;
--- index-react.js
+++ index-react.js
@@ -1,15 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-// const notifItems = notifications.map((notifications) =>
-  
-//   <div>
-//   	<h1>{notifications.title}</h1>
-//   	<p>{notifications.body}</p>
-//   </div>
-
-// );
-
 const numbers = [1, 2, 3, 4, 5];
 const listItems = numbers.map((numbers) =>
   <li>{numbers}</li>
